1c975027cadaba0b6e0203c4527ce0c5734ee8d8
Hoàng Minh Thắng <p@banphim.net>
2013-11-28 12:10:10 +0700

    add test make-filter's functionality

--- test/leiningen/cl2c_test.clj
+++ test/leiningen/cl2c_test.clj
@@ -14,3 +14,16 @@
         (and (or (re-find (re-pattern ".cl2$") ~'s)
                  (re-find (re-pattern ".hic$") ~'s))
              (re-find (re-pattern "match-me") ~'s))))
+
+(facts "filter files with .cl2 or .hic extensions and has
+  \"match-me\" in its name."
+  (filter (make-filter '(and (or ".cl2$" ".hic$")
+                             "match-me"))
+          ["dir/match-me.cl2"
+           "dir/match-me.hic"
+           "dir/match-me.js"
+           "dir/match-me.html"
+           "dir/match-no-me.clj"
+           "dir/match-no-me.hic"])
+  => ["dir/match-me.cl2"
+      "dir/match-me.hic"])
